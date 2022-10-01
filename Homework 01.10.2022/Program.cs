ae4f271acfed003c35813305b5e3dd5d3c8293d4
unknown <javidra@code.edu.com>
2022-10-01 19:04:23 +0400

    01.10.2022 Homework

---
+++ Homework 01.10.2022/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Homework_01._10._2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   //================================================================
+            // n ededi gelir. N ededinin sade ve ya murekkeb oldugunu tapin.
+            // Taski bitirib githuba qoyub repositorinin linkini turn in edin.
+            //================================================================
+            int n = 6;
+            if (n > 1)
+            {
+                if (n == 2 || n == 3 || n == 5 || n == 7)
+                {
+                    Console.WriteLine("Sadedir");
+                }
+                else
+                {
+                    if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+                    {
+                        Console.WriteLine("Murekkebdir");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sadedir");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
